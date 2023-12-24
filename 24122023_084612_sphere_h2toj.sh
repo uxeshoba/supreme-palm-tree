f75bc41e91e46f73695add427e92a60cf70c025e
uxeshoba <ziradzigua@gmail.com>
2023-12-24 08:46:12 +0000

    Fixing sphere

---
+++ 24122023_084612_sphere_h2toj.sh
@@ -0,0 +1,1 @@
+The name for Ivory Soap was inspired by a verse from the Bible. Harley Proctor got though of the name when the minister read from Psalms 45:8, "All thy garments smell of myrrh and aloes and cassia, out of the ivory palaces whereby they have made thee glad."
