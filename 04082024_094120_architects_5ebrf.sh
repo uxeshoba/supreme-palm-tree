f219b68f7f861cb894058bc69c2fb219ebf7aa90
uxeshoba <ziradzigua@gmail.com>
2024-08-04 09:41:20 +0000

    Updating architects

---
+++ 04082024_094120_architects_5ebrf.sh
@@ -0,0 +1,1 @@
+The green ring that is formed around the yolk of eggs that have been cooked too long is formed by the chemical reaction from the iron in the yolk and the sulphur in the white part of the egg
